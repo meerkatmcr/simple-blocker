eac81e1bfdd7bc67742eabbf55542a22b67f0187
Stephen Smith <stephen.smith@jointhedotsmr.com>
2019-12-11 16:18:20 +0000

    Make action methods fluent

--- src/Traits/Blockable.php
+++ src/Traits/Blockable.php
@@ -6,11 +6,13 @@ trait Blockable {
     public function block()
     {
         $this->blocked_at = now();
+        return $this;
     }
 
     public function unblock()
     {
         $this->blocked_at = null;
+        return $this;
     }
 
     public function isBlocked()
